1b0f41c3e90f12d5de31c6f52899598ced077e58
ibrarLexumsoft <muhammad.ibrar@lexumsoft.com>
2025-10-16 21:39:26 +0500

    workin on login ui

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,10 +1,37 @@
 import React from "react";
+import Label from "../../components/common/Label";
+import AppInput from "../../components/common/AppInput";
+import AppButton from "../../components/common/AppButton";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <div className="flex items-center justify-center h-full h-screen">
+        <div className="p-5 bg-white rounded-md w-1/2 m-auto ">
+          <div className="text-center">
+            <strong className="text-xl">Welcom Back</strong>
+            <p>Please enter your details</p>
+          </div>
+
+          <div className="form">
+            <form action="">
+              <div>
+                <Label text="Email" />
+                <AppInput />
+              </div>
+              <div>
+                <Label text="Password" />
+                <AppInput type="password" />
+              </div>
+
+              <AppButton text="Login" />
+              <p>Forgot Pasword</p>
+              <p>create new account</p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
